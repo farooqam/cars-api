3804fb443acfd44836cd2c29797f9ac48759a007
Farooq Mahmud <farooq_am@hotmail.com>
2019-04-14 12:34:26 -0700

    wip

--- src/controllers/baseController.js
+++ src/controllers/baseController.js
@@ -5,13 +5,35 @@ const halson = require('halson');
 class BaseController {
     constructor() {
         this.actions = [];
-        this.server = null;
     }
 
     setupActions(app, swagger) {
-        this.server = app;
         this.actions.forEach(action => {
-            
-        })
+            const spec = action['spec'];
+            const methodName = spec['method']
+            //logger.info(`Setting up swagger for ${spec['nickname']} (${methodName})`
+            swagger['add' + methodName](action);
+            app[methodName.toLowerCase()](spec['path'], spec['path'], action['action']);
+        });
     }
+
+    addAction(spec, fn) {
+        const newAction = {spec, action: fn.bind(this)};
+        this.actions.push(newAction);
+    }
+
+    logRestError(type, msg) {
+        //logger.error(`Error ${type}: ${msg}`);
+
+        if (errors[type]) {
+            return new errors[type](msg);
+        } 
+
+        return {error: true, type, msg}
+    }
+
+    writeHal(res, obj) {
+
+    }
+
 }
