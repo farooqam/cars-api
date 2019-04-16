46be3a4e558367f7920ad0d23168f16ae3f2bc10
Farooq Mahmud <farooq_am@hotmail.com>
2019-04-16 09:23:54 -0700

    Get makes from database.

--- src/models/constants.js
+++ src/models/constants.js
@@ -1,5 +1,12 @@
 const MEDIUMSTRING = 128;
 
+const QUERYOPTIONS = {
+    EXCLUDE_METADATA_ATTRIBUTES: {
+        _id: 0, __v: 0, createdAt: 0, updatedAt: 0,
+    },
+};
+
 module.exports = {
     MEDIUMSTRING,
+    QUERYOPTIONS,
 };
