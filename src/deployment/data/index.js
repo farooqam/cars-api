83828315d01748dfb66b327dd868733662902522
Farooq Mahmud <farooq.mahmud@marel.com>
2019-04-15 23:28:43 -0700

    wip

--- src/deployment/data/index.js
+++ src/deployment/data/index.js
@@ -1,17 +1,4 @@
 /* eslint-disable global-require */
-const makes = require('../data/makes');
-
 module.exports = [
-    {
-        model: require('../../models/make'),
-        name: 'make',
-        friendlyName: 'Makes',
-        values: makes,
-        mapping(m) {
-            return {
-                name: m.name,
-                countryOfOrigin: m.countryOfOrigin,
-            };
-        },
-    },
+    require('../data/makes'),
 ];
--- src/deployment/data/makes.js
+++ src/deployment/data/makes.js
@@ -13,4 +13,16 @@ const makes = [
     },
 ];
 
-module.exports = makes;
+module.exports = {
+    // eslint-disable-next-line global-require
+    model: require('../../models/make'),
+    name: 'make',
+    friendlyName: 'Makes',
+    values: makes,
+    mapping(model) {
+        return {
+            name: model.name,
+            countryOfOrigin: model.countryOfOrigin,
+        };
+    },
+};
